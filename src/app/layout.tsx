4b8167266522b898b021aa4cf0ed2489b570ceb4
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-02 19:06:08 +0700

    Updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import { ReactNode } from "react";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Gluoin Code | My Personal Portofolio",
@@ -12,6 +13,7 @@ const RootLayout = ({ children }: { children: ReactNode }) => {
     <html lang="en">
       <body className="bg-theme text-theme">
         <div className="max-w-7xl mx-auto my-auto">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
