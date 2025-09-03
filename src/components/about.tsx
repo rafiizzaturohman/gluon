02512b61d13a3c4ea276160c46d806053d1e281b
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-03 13:58:00 +0700

    About Update

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -10,19 +10,11 @@ const AboutMe = () => {
 
         <div className="space-y-4">
           <p className="card-text">
-            I'm a Web Developer from Bandung, Indonesia, with a passion for
-            creating functional, user-friendly, and visually appealing websites.
-            I enjoy learning new things and constantly exploring ways to improve
-            my skills in the ever-evolving tech world.
+            I'm a Web Developer from Bandung, Indonesia, with a passion for creating functional, user-friendly, and visually appealing websites. I enjoy learning new things and constantly exploring ways to improve my skills in the ever-evolving tech world.
           </p>
 
           <p className="card-text">
-            In addition to web development, I have practical experience in light
-            computer services, such as reinstalling operating systems, upgrading
-            RAM, and replacing storage devices like HDDs, SSDs, and NVMe drives.
-            My goal is to combine technical expertise with curiosity and
-            creativity to provide solutions that not only work well but also
-            provide real value to users.
+            In addition to web development, I have practical experience in light computer services, such as reinstalling operating systems, upgrading RAM, and replacing storage devices like HDDs, SSDs, and NVMe drives. My goal is to combine technical expertise with curiosity and creativity to provide solutions that not only work well but also provide real value to users.
           </p>
         </div>
 
