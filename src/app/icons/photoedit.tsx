1b617fba7abcaf2fcdc310c95a7c29808dfc2f46
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-14 06:43:20 +0700

    Content Update

--- src/app/icons/photoedit.tsx
+++ src/app/icons/photoedit.tsx
@@ -5,9 +5,8 @@ const PhotoEdit = ({ className }: { className?: string }) => {
       viewBox="0 0 280 176"
       shapeRendering="geometricPrecision"
       textRendering="geometricPrecision"
-      className={`${className} fill-current`}
+      className="w-16 h-16 text-orange-500"
     >
-      {/* Frame */}
       <rect
         width="271.044993"
         height="168.359942"
@@ -15,61 +14,55 @@ const PhotoEdit = ({ className }: { className?: string }) => {
         ry="12"
         transform="matrix(.918714 0 0 0.918714 15.493586 10.662682)"
         fill="none"
+        fillOpacity="0"
         stroke="currentColor"
         strokeWidth="10"
       />
-
-      {/* Isi gambar */}
       <g>
-        <g transform="translate(8.000001 0.000001)">
-          <rect
-            width="175.567567"
-            height="27.243243"
-            rx="5"
-            ry="5"
-            transform="matrix(1.163801 0 0 0.7963 27.49481 121.953102)"
-          />
-          <rect
-            width="175.567567"
-            height="27.243243"
-            rx="5"
-            ry="5"
-            transform="matrix(.926937 0 0 0.7963 69.080447 111.106205)"
-          />
-        </g>
-        <g transform="translate(9.487251 0)">
-          <polygon
-            points="12.041008,-51.372646 29.238928,-4.812692 29.238928,16.881103 0,16.881103 -29.238928,16.881103 -17.503949,-4.812692 12.041008,-51.372646"
-            transform="translate(63.706732 126.765794)"
-          />
-          <polygon
-            points="18.896593,-76.460993 45.18361,-4.812692 29.238928,16.881103 0,16.881103 -29.238928,16.881103 -18.653445,-4.812692 18.896593,-76.460993"
-            transform="translate(122.184588 126.765794)"
-          />
-          <polygon
-            points="23.487178,-93.896018 45.18361,-4.812692 29.238928,16.881103 0,16.881103 -29.238928,16.881103 -18.653445,-4.812692 23.487178,-93.896018"
-            transform="translate(176.689412 126.765794)"
-          />
-        </g>
+        <path
+          d="M33.31381,143.6469c-3.21375,0-5.81901-1.78258-5.81901-3.9815v-13.73079c0-2.19892,2.60526-3.9815,5.81901-3.9815h13.18621L75.74774,72.26627l11.09991,38.83995h22.36823l31.86531-60.8014l22.21064,60.53767L200.1766,32.8698l19.05465,78.23643h7.9546c2.55967,0,4.63469,1.78258,4.63469,3.9815v13.73079c0,.00078,0,.00156,0,.00234v10.84455c0,2.19892-2.60526,3.9815-5.81901,3.9815L33.31381,143.6469Z"
+          fillOpacity="0"
+          stroke="currentColor"
+          strokeWidth="6"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
         <ellipse
           rx="14.306942"
           ry="14.306942"
-          transform="translate(77.080448 50.304801)"
+          transform="translate(77.298422 47.035198)"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="6"
         />
       </g>
-
-      {/* Icon Pensil */}
-      <g transform="matrix(1.184015 0.887865-.887865 1.184015 343.131046 68.834292)">
+      <g>
         <path
           d="M0,55.616314L0,5C0,2.238576,2.238576,0,5,0h21.265256c2.761424,0,5,2.238576,5,5v50.616314"
-          transform="matrix(1.050756 0 0 1.001686-56.786199 39.571131)"
+          transform="matrix(.772774 0.772774-.708299 0.708299 245.504425 89.540652)"
+          fill="none"
+          stroke="none"
+          strokeWidth="6"
+        />
+        <polygon
+          points="-0.004195,-16.150728 13.97855,8.072495 -13.986941,8.072495 -0.004195,-16.150728"
+          transform="matrix(-.751113-.749803 0.72652-.727789 210.212145 149.071267)"
+          fill="none"
+          stroke="none"
+          strokeWidth="6"
+        />
+      </g>
+      <g>
+        <path
+          d="M0,50.322911L0,4.524114C0,2.025515,2.025905,0,4.524987,0h19.245002c2.499081,0,4.524987,2.025515,4.524987,4.524114l-.035592,45.796753"
+          transform="matrix(.742997 0.742997-.762418 0.762418 245.892666 92.783209)"
           fill="currentColor"
           stroke="#1c1a18"
           strokeWidth="6"
         />
-        <path
-          d="M0,0h31.265256L15.632628,55.981958v0L0,0Z"
-          transform="matrix(1 0 0 0.452621-55.992749 96.614494)"
+        <polygon
+          points="-0.004195,-16.150728 13.97855,8.072495 -13.986941,8.072495 -0.004195,-16.150728"
+          transform="matrix(-.644206-.643083 0.623124-.624212 211.133185 148.56909)"
           fill="currentColor"
           stroke="#1c1a18"
           strokeWidth="6"
