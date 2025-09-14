5484e49e268ef3a2564195c40eb3d0510b46f86c
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-14 08:33:50 +0700

    Content Update

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -2,20 +2,20 @@ import { BookIcon } from "@/app/icons";
 
 const educationData = [
   {
-    title: "University School Of The Arts",
-    period: "2007 — 2008",
+    title: "Yadika Soreang Vocational School",
+    period: "2019 — 2022",
     description:
       "Nemo enims ipsam voluptatem, blanditiis praesentium voluptum delenit atque corrupti, quos dolores et quas molestias exceptur.",
   },
   {
-    title: "New York Academy Of Art",
-    period: "2006 — 2007",
+    title: "Rubicamp",
+    period: "Oct 2022 — Dec 2022",
     description:
       "Ratione voluptatem sequi nesciunt, facere quisquams facere menda ossimus, omnis voluptas assumenda est omnis..",
   },
   {
-    title: "High School Of Art And Design",
-    period: "2002 — 2004",
+    title: "STMIK Mardira Indonesia",
+    period: "2025 — Now",
     description:
       "Duis aute irure dolor in reprehenderit in voluptate, quila voluptas mag odit aut fugit, sed consequuntur magni dolores eos.",
   },
@@ -24,7 +24,15 @@ const educationData = [
 const MyResume = () => {
   return (
     <section className="content-card">
-      <div></div>
+      <div>
+        {educationData.map((eduItem, index) => (
+          <div key={index}>
+            <h6>{eduItem.title}</h6>
+            <p className="text-amber-500">{eduItem.period}</p>
+            <p>{eduItem.description}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
