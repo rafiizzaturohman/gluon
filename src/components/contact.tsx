4159907d59308abceffd369182c9d7dc2383edf8
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-24 16:04:05 +0700

    Embedded Maps Update

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -8,6 +8,27 @@ const MyContact = () => {
       <h1 className="main-page-title">Contact</h1>
 
       <Maps />
+
+      <section id="contact-input" className="space-y-6">
+        <div className="flex flex-col space-y-6 md:space-y-0 md:flex-row w-full space-x-2">
+          <input
+            type="text"
+            className="border-2 border-amber-500/50 focus:ring-2 focus:ring-amber-600 rounded-lg p-2 h-auto w-full transition-all ease-in-out delay-75 duration-1000"
+          />
+          <input
+            type="text"
+            className="border-2 border-amber-500/50 focus:ring-2 focus:ring-amber-600 rounded-lg p-2 h-auto w-full transition-all ease-in-out delay-75 duration-1000"
+          />
+        </div>
+
+        <div>
+          <textarea
+            name=""
+            id=""
+            className="border-2 border-amber-500/50 focus:ring-2 focus:ring-amber-600 rounded-lg p-2 h-48 w-full transition-all ease-in-out delay-75 duration-1000"
+          ></textarea>
+        </div>
+      </section>
     </section>
   );
 };
