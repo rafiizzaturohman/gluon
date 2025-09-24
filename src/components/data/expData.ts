14b86f0799f7ac8850768f67ea3a6dc320fb7073
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-24 17:30:07 +0700

    Embedded Maps Update

--- src/components/data/expData.ts
+++ src/components/data/expData.ts
@@ -3,13 +3,13 @@ const experienceData = [
     title: "Admin & Packer Online Shop",
     period: "Jan 2023 — May 2025",
     description:
-      "Finished my journey in Software Engineering, gaining skills in coding, app development, and software fundamentals.",
+      "Worked as admin and packer, focusing on order management and customer satisfaction.",
   },
   {
     title: "Coding & multimedia teacher",
     period: "Jun 2022 — Now",
     description:
-      "Took part in a bootcamp as a JavaScript Fullstack Developer, sharpening my skills in building scalable and dynamic web applications.",
+      "Teaching coding and multimedia, helping students build skills in programming, design, and creative projects.",
   },
 ];
 
