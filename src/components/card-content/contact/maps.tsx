81e3a2f6988b4f33cb40eec2ff0a906b9f3be7ee
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-24 14:40:46 +0700

    Maps Update

--- src/components/card-content/contact/maps.tsx
+++ src/components/card-content/contact/maps.tsx
@@ -1,8 +1,15 @@
 const Maps = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <section className="mapbox">
+      <figure>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31676.50161681151!2d107.50665678428743!3d-7.060556192388614!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68ecf3e940f8af%3A0x7b5058565a1cd93e!2sKec.%20Cangkuang%2C%20Kabupaten%20Bandung%2C%20Jawa%20Barat!5e0!3m2!1sen!2sus!4v1758698716520!5m2!1sen!2sus"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </figure>
+    </section>
   );
 };
 
--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,29 +1,13 @@
 "use client";
 
+import Maps from "@/components/card-content/contact/maps";
+
 const MyContact = () => {
   return (
     <section className="space-y-6">
-      <div className="space-y-6">
-        <h1 className="main-page-title">Contact</h1>
+      <h1 className="main-page-title">Contact</h1>
 
-        <div className="space-y-4">
-          <blockquote className="card-text">
-            I&apos;m a Web Developer from Bandung, Indonesia, with a passion for
-            creating functional, user-friendly, and visually appealing websites.
-            I enjoy learning new things and constantly exploring ways to improve
-            my skills in the ever-evolving tech world.
-          </blockquote>
-
-          <blockquote className="card-text">
-            In addition to web development, I have practical experience in light
-            computer services, such as reinstalling operating systems, upgrading
-            RAM, and replacing storage devices like HDDs, SSDs, and NVMe drives.
-            My goal is to combine technical expertise with curiosity and
-            creativity to provide solutions that not only work well but also
-            provide real value to users.
-          </blockquote>
-        </div>
-      </div>
+      <Maps />
     </section>
   );
 };
