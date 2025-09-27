58e70501d0277d7d2c3bfeb7f675310b5613bdfd
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2025-09-27 19:17:02 +0700

    Portofolio Updated

---
+++ src/components/data/portofolioData.ts
@@ -0,0 +1,19 @@
+import POS from "../../../public/image/portofolio/POS.png";
+import pustami from "../../../public/image/portofolio/pustami.png";
+
+const portfolioItems = [
+  {
+    title: "Point of Sales",
+    subtitle: "Web Application",
+    image: POS,
+    alt: "POS",
+  },
+  {
+    title: "Pustami",
+    subtitle: "Mobile App Design",
+    image: pustami,
+    alt: "Pustami",
+  },
+];
+
+export default portfolioItems;
