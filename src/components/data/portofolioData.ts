415359d80d76585807d8110e0ea523f09c41b0c6
rafiizzaturohman <rafiizzaturohman06@gmail.com>
2026-02-13 18:26:09 +0700

    Portofolio Update

--- src/components/data/portofolioData.ts
+++ src/components/data/portofolioData.ts
@@ -1,5 +1,7 @@
 import POS from "../../../public/image/portofolio/POS.png";
 import pustami from "../../../public/image/portofolio/pustami.png";
+import phonebook from "../../../public/image/portofolio/Responsive.gif";
+import tempdity from "../../../public/image/portofolio/TempDity.png";
 
 const portfolioItems = [
   {
@@ -7,12 +9,28 @@ const portfolioItems = [
     subtitle: "Web Application",
     image: POS,
     alt: "POS",
+    link: "https://github.com/rafiizzaturohman/PointOfSales.git",
   },
   {
     title: "Pustami",
     subtitle: "Mobile App Design",
     image: pustami,
     alt: "Pustami",
+    link: "https://www.figma.com/design/FGNbCPoiAhvxgiL67lZiYM/PUSTAMI?node-id=0-1&p=f&t=hQQPRF8v2SQPziPO-0",
+  },
+  {
+    title: "Phonebook",
+    subtitle: "Web Application",
+    image: phonebook,
+    alt: "Phonebook",
+    link: "https://github.com/rafiizzaturohman/HooksPhonebook.git",
+  },
+  {
+    title: "TempDity",
+    subtitle: "Web Application",
+    image: tempdity,
+    alt: "tempdity",
+    link: "https://github.com/rafiizzaturohman/TempDity-Laravel.git",
   },
 ];
 
